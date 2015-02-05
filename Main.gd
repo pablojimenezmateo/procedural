df99df616ab1c3d06d051e4ed2a78af8841a9df4
Pablo Jiménez Mateo <pablojimenezmateo@gmail.com>
2015-02-05 20:16:52 +0100

    Quaternions almost working

--- Main.gd
+++ Main.gd
@@ -110,8 +110,9 @@ var yaw = 0
 var pitch = 0
 
 func _input(event):
-	var view_sensitivity = 0.3
+	var view_sensitivity = 0.003
 	var camera = get_node("ghost_cam")
+	var cube = get_node("TestCube")
 	
 	if (event.type == InputEvent.MOUSE_MOTION):
 	
@@ -122,9 +123,48 @@ func _input(event):
 		#camera.set_rotation(Vector3(0, deg2rad(yaw), 0)) #Moves left and right
 		#camera.set_rotation(Vector3(deg2rad(pitch), 0, 0)) #Moves up and down
 
+		#cube.set_rotation(Vector3(deg2rad(pitch), deg2rad(yaw), 0)) #Moves left and right
+		#cube.set_rotation(Vector3(0, deg2rad(yaw), 0)) #Moves left and right
+		#cube.set_rotation(Vector3(deg2rad(pitch), 0, 0)) #Moves up and down
+
 		#var origin = camera.get_global_transform().origin
 		#var basis = camera.get_global_transform().basis
 		
-		var transform = camera.get_global_transform().rotated(Vector3(0, 1, 0), deg2rad(yaw))
-		var transform = camera.get_global_transform().rotated(Vector3(1, 0, 0), deg2rad(pitch))
-		camera.set_global_transform(transform)
+		var q = Quat()
+
+#		var cross = cube.get_global_transform().origin.cross(Vector3(0.1,0.1,0))
+#		
+#		print(cube.get_global_transform().origin, " Basis: ", cube.get_global_transform().basis[0])
+#		q.x = cross[0];
+#		q.y = cross[1];
+#		q.z = cross[2];
+#		q.w = sqrt(cube.get_global_transform().origin.length_squared() * Vector3(0.1,0.1,0).length_squared()) + cube.get_global_transform().origin.dot(Vector3(0.1,0.1,0))
+#		
+#		q.w = 0.9239
+#		q.x = 0
+#		q.y = 0.3827
+#		q.z = 0
+#		q = q.normalized()
+		
+		print(q)
+		
+		var c1 = cos(yaw/2)
+		var c2 = cos(pitch/2)
+		var c3 = 1
+		var s1 = sin(yaw/2)
+		var s2 = sin(pitch/2)
+		var s3 = 0
+		q.w = c1 * c2 * c3 - s1 * s2 * s3
+		q.x = s1 * s2 * c3 + c1 * c2 * s3
+		q.y = s1 * c2 * c3 + c1 * s2 * s3
+		q.z = c1 * s2 * c3 - s1 * c2 * s3
+		q = q.normalized()
+		
+		print(q)
+
+		#var transform = camera.get_global_transform().rotated(q)
+		#var transform = camera.get_global_transform().rotated(Vector3(1, 0, 0), deg2rad(pitch))
+		#var trans = camera.get_translation()
+		cube.set_global_transform(Transform(q))
+
+		#camera.set_translation(trans)
