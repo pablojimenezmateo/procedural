66bc707b8a2452ba552185b7f0bb244e682f608b
Pablo Jiménez Mateo <pablojimenezmateo@gmail.com>
2015-02-05 22:17:42 +0100

    So close?

--- Main.gd
+++ Main.gd
@@ -9,6 +9,7 @@ extends Spatial
 func _ready():
 	set_process(true)
 	set_process_input(true)
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		
 func _process(delta):
 #
@@ -104,6 +105,9 @@ func _process(delta):
 		camera.set_translation(camera.get_translation() + camera.get_global_transform().basis[0] * delta* camSpeed)
 		#cube.set_translation(cube.get_translation() + cube.get_global_transform().basis[0] * delta* camSpeed)
 		
+	if Input.is_action_pressed("quit"):
+		get_tree().quit()
+		
 
 
 var yaw = 0
@@ -116,55 +120,55 @@ func _input(event):
 	
 	if (event.type == InputEvent.MOUSE_MOTION):
 	
-		yaw = fmod(yaw - event.relative_x * view_sensitivity, 360)
-		pitch = max(min(pitch - event.relative_y * view_sensitivity, 90), -90)
-		
-		#camera.set_rotation(Vector3(deg2rad(pitch), deg2rad(yaw), 0)) #Moves left and right
-		#camera.set_rotation(Vector3(0, deg2rad(yaw), 0)) #Moves left and right
-		#camera.set_rotation(Vector3(deg2rad(pitch), 0, 0)) #Moves up and down
-
-		#cube.set_rotation(Vector3(deg2rad(pitch), deg2rad(yaw), 0)) #Moves left and right
-		#cube.set_rotation(Vector3(0, deg2rad(yaw), 0)) #Moves left and right
-		#cube.set_rotation(Vector3(deg2rad(pitch), 0, 0)) #Moves up and down
-
-		#var origin = camera.get_global_transform().origin
-		#var basis = camera.get_global_transform().basis
-		
-		var q = Quat()
-
-#		var cross = cube.get_global_transform().origin.cross(Vector3(0.1,0.1,0))
+		yaw = yaw - event.relative_x * view_sensitivity #Horizontal
+		pitch = pitch - event.relative_y * view_sensitivity #Vertical
+				
+		#The basic explanation with examples can be found here
+		#http://www.euclideanspace.com/maths/geometry/rotations/conversions/eulerToQuaternion/steps/index.htm
+		#This helps a lot http://wiki.alioth.net/index.php/Quaternion
+#		var c1 = cos(yaw/2)
+#		var c2 = cos(pitch/2)
+#		var c3 = 1
+#		var s1 = sin(yaw/2)
+#		var s2 = sin(pitch/2)
+#		var s3 = 0
 #		
-#		print(cube.get_global_transform().origin, " Basis: ", cube.get_global_transform().basis[0])
-#		q.x = cross[0];
-#		q.y = cross[1];
-#		q.z = cross[2];
-#		q.w = sqrt(cube.get_global_transform().origin.length_squared() * Vector3(0.1,0.1,0).length_squared()) + cube.get_global_transform().origin.dot(Vector3(0.1,0.1,0))
-#		
-#		q.w = 0.9239
-#		q.x = 0
-#		q.y = 0.3827
-#		q.z = 0
+#		q.w = c1 * c2 * c3 - s1 * s2 * s3
+#		q.x = s1 * s2 * c3 + c1 * c2 * s3
+#		q.y = s1 * c2 * c3 + c1 * s2 * s3
+#		q.z = c1 * s2 * c3 - s1 * c2 * s3
 #		q = q.normalized()
 		
-		print(q)
+		#Yeah!, maths!
+		var qy = Quat()
+		var qx = Quat()
+		var q = Quat()
 		
-		var c1 = cos(yaw/2)
-		var c2 = cos(pitch/2)
-		var c3 = 1
-		var s1 = sin(yaw/2)
-		var s2 = sin(pitch/2)
-		var s3 = 0
-		q.w = c1 * c2 * c3 - s1 * s2 * s3
-		q.x = s1 * s2 * c3 + c1 * c2 * s3
-		q.y = s1 * c2 * c3 + c1 * s2 * s3
-		q.z = c1 * s2 * c3 - s1 * c2 * s3
+		#Create the quaternion for the Y axis movement
+		qy.w = cos(yaw/2)
+		qy.x = sin(yaw/2) * 0
+		qy.y = sin(yaw/2) * 1
+		qy.z = sin(yaw/2) * 0
+		qx = qx.normalized()
+		
+		#Create the quaternion for the X axis movement
+		qx.w = cos(pitch/2)
+		qx.x = sin(pitch/2) * 1
+		qx.y = sin(pitch/2) * 0
+		qx.z = sin(pitch/2) * 0
+		qx = qx.normalized()
+		
+		#Get the final quaternion http://en.wikipedia.org/wiki/Slerp
+		q = qy.slerp(qx, 0.5)
 		q = q.normalized()
 		
-		print(q)
-
-		#var transform = camera.get_global_transform().rotated(q)
-		#var transform = camera.get_global_transform().rotated(Vector3(1, 0, 0), deg2rad(pitch))
+		print(yaw, " ### ", q)
+		
+		#The transformation moves the camera to the origin, so we need to store its previous position to move it back
 		#var trans = camera.get_translation()
-		cube.set_global_transform(Transform(q))
-
+		
+		#Rotate the camera
+		camera.set_global_transform(Transform(q))
+		
+		#Move it back
 		#camera.set_translation(trans)
